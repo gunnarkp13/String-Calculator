9bdbbf6a2c9077bf3a548dcbe41813eec5ab0ec7
GunniKalli <gunnarkp13@ru.is>
2014-10-11 14:52:39 +0000

    Notepad ++ synir ekki error a String

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,7 +2,7 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 	
-	public int add(string value){
+	public int add(String value){
 		if(value.equals("")){return 0;}
 		else {return 1;}
 	}
