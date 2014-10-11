f02fcfc88682d27831cd000aa02bb3f3d83f0a10
GunniKalli <gunnarkp13@ru.is>
2014-10-11 14:41:40 +0000

    bjó til fyrsta test

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,5 +1,5 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
-
+	
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -4,5 +4,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class CalculatorTest {
-
+	public static void main(String args[]){
+	org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+	}
+	
+	// Prófa tómastrenginn viljum fá 0 útúr fallinu.
+	@Test
+	public void testEmptyString(){
+		assertEquals(0,Calculator.add(""));
+	}
 }
