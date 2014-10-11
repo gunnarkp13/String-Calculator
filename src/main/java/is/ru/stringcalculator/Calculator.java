3530bca0b6286338b5389b835060e9afb63472c6
GunniKalli <gunnarkp13@ru.is>
2014-10-11 16:12:52 +0000

    revised 2 tölur

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
 	public static int add(String value){
 		if(value.equals("")){return 0;}
 		else if(value.contains(",")){
-			int[2] numbers = (int) value.split(",");
+			int[] numbers = new int[value.split(",").length];
 			return numbers[0] + numbers[1];
 		}
 		else {return 1;}
